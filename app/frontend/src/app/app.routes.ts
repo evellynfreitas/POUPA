faf3d6a5fc27cc5e5cfd9313720e10071deef591
Rafael Tefé <tefe.rafael@gmail.com>
2025-01-18 20:24:10 -0300

    Finaliza template das páginas de login e signup

--- app/frontend/src/app/app.routes.ts
+++ app/frontend/src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
+import { SignupComponent } from './features/signup/signup.component';
+import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
   {
     path: "entrar",
     pathMatch: "full",
     component: LoginComponent
+  },
+  {
+    path: "cadastro",
+    pathMatch: "full",
+    component: SignupComponent
   }
 ];
